01884bae719751692c5db9b00541396bcac3301f
Kate Adair <adairkate@gmail.com>
2017-09-13 12:30:27 +0800

    Add route and view for list page

--- todo.rb
+++ todo.rb
@@ -15,19 +15,23 @@ get '/' do
   redirect '/lists'
 end
 
-# Render the new list form
+# View list of lists
 get '/lists' do
   @lists = session[:lists]
   erb :lists, layout: :layout
 end
 
-# Return an error message if the name is invalid. Return nil if name is valid.
-def error_for_list_name(name)
-  if !(1..100).cover? name.size
-    'The list name must be between 1 and 100 characters.'
-  elsif session[:lists].any? { |list| list[:name] == name }
-    'List name must be unique.'
-  end
+# Render the new list form
+get '/lists/new' do
+  erb :new_list, layout: :layout
+end
+
+# View list of todos
+get '/lists/:id' do
+  id = params[:id].to_i
+  @list = session[:lists][id]
+  
+  erb :list, layout: :layout
 end
 
 # Create a new list
@@ -39,12 +43,17 @@ post '/lists' do
     erb :new_list, layout: :layout
   else
     session[:lists] << { name: list_name, todos: [] }
+    session[:id] += 1
     session[:success] = 'The list has been created.'
     redirect '/lists'
   end
 end
 
-# Render the new list form
-get '/lists/new' do
-  erb :new_list, layout: :layout
+# Return an error message if the name is invalid. Return nil if name is valid.
+def error_for_list_name(name)
+  if !(1..100).cover? name.size
+    'The list name must be between 1 and 100 characters.'
+  elsif session[:lists].any? { |list| list[:name] == name }
+    'List name must be unique.'
+  end
 end
